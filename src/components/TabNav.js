33772ce6115a9c9a25074ce73d2688ee3c48b18f
Barbara Mayfield <bmayfield1991@gmail.com>
2019-09-13 21:00:44 -0500

    final commit finished TabNav

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -11,5 +11,26 @@ import { NavLink } from "react-router-dom";
 // https://react.semantic-ui.com/collections/breadcrumb/
 
 export default function TabNav() {
+    return (
+    <Menu>
+        <Menu.Item>
+            <NavLink to={`/character`} activeClassName="active">
+                Characters
+            </NavLink>
+        </Menu.Item>
+        
+        <Menu.Item>
+            <NavLink to={`/location`} activeClassName="active">
+                Locations
+            </NavLink>
+        </Menu.Item>
 
+        <Menu.Item>
+            <NavLink to={`/episode`} activeClassName="active">
+                Episodes
+            </NavLink>
+        </Menu.Item>
+
+      </Menu>
+    )
 };
